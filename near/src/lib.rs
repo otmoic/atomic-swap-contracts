f4d6f8388ee328cd10c154bcdfe7d7c32e2f06c5
Antonio Yang <yanganto@gmail.com>
2022-07-25 19:56:01 +0800

    Initialize near project

---
+++ near/src/lib.rs
@@ -0,0 +1,77 @@
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::{log, near_bindgen, AccountId, Balance};
+
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct Contract {}
+
+impl Default for Contract {
+    fn default() -> Self {
+        Self {}
+    }
+}
+
+#[near_bindgen]
+impl Contract {
+    pub fn transfer_out(
+        &self,
+        sender: AccountId,
+        _bridge: AccountId,
+        _token: AccountId,
+        _amount: Balance,
+        _hashlock: [u8; 32],
+        _timelock: u64,
+        _dst_chain_id: u64,
+        _dst_address: AccountId,
+        _bid_id: u64,
+    ) {
+        log!("transfer out from {}", sender);
+    }
+    pub fn transfer_in(
+        &self,
+        sender: AccountId,
+        _dst_address: AccountId,
+        _token: AccountId,
+        _amount: Balance,
+        _hashlock: [u8; 32],
+        _timelock: u64,
+        _src_chain_id: u64,
+        _src_transfer_id: [u8; 32],
+    ) {
+        log!("transfer in from {}", sender);
+    }
+    pub fn confirm(
+        &self,
+        sender: AccountId,
+        _receiver: AccountId,
+        _token: AccountId,
+        _amount: Balance,
+        _hashlock: [u8; 32],
+        _timelock: u64,
+        _preimage: [u8; 32],
+    ) {
+        log!("confirm with {}", sender);
+    }
+    pub fn refund(
+        &self,
+        sender: AccountId,
+        _receiver: AccountId,
+        _token: AccountId,
+        _amount: Balance,
+        _hashlock: [u8; 32],
+        _timelock: u64,
+    ) {
+        log!("refund to {}", sender);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn get_default_greeting() {
+        let contract = Contract::default();
+        assert_eq!(contract.transfer_out(), ());
+    }
+}
