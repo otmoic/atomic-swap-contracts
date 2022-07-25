4a7046cd6313687fc4e9a192ca02775877424b69
Antonio Yang <yanganto@gmail.com>
2022-07-25 20:35:29 +0800

    Add dep check

--- flake.nix
+++ flake.nix
@@ -4,25 +4,33 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
+    dependency-refresh.url = "github:yanganto/dependency-refresh";
   };
 
-  outputs = { self, rust-overlay, nixpkgs, flake-utils }:
+  outputs = { self, rust-overlay, nixpkgs, flake-utils, dependency-refresh }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-        rust = pkgs.rust-bin.stable.latest.default.override {
-          # extensions = [ "rust-src" ];
-          # targets = [ "wasm32-unknown-unknown" ];
-        };
+        rust = pkgs.rust-bin.stable.latest.default;
+        dr = dependency-refresh.defaultPackage.${system};
+        updateDependencyScript = pkgs.writeShellScriptBin "update-dependency" ''
+          dr ./$1/Cargo.toml
+          if [ -f "Cargo.toml.old" ]; then
+            rm Cargo.toml.old
+            exit 1
+          fi
+        '';
       in
       with pkgs;
       {
         devShell = mkShell {
           buildInputs = [
             rust
+            dr
+            updateDependencyScript
           ];
         };
       }
