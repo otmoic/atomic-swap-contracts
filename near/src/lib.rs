ca58655dd94b9ebbfcaf53f42ba40047b4d43365
Antonio Yang <yanganto@gmail.com>
2022-07-29 16:55:01 +0800

    Add unit test on lock

--- near/src/lib.rs
+++ near/src/lib.rs
@@ -99,10 +99,7 @@ impl Contract {
                 *transfer_status == TransferStatus::Pending,
                 "not pending transfer"
             );
-            require!(
-                hashlock == keccak256_preimage(secret_key),
-                "incorrect secret_key"
-            );
+            require!(try_lock(secret_key, hashlock), "incorrect secret_key");
 
             *transfer_status = TransferStatus::Confirmed;
 
@@ -164,9 +161,17 @@ fn keccak256(
     out
 }
 
-fn keccak256_preimage(preimage: [u8; 32]) -> [u8; 32] {
+fn try_lock(secret_key: SecretKey, hashlock: HashLock) -> bool {
     let mut hasher = Sha3_256::new();
-    hasher.update(preimage);
+    hasher.update(secret_key);
+    let result = hasher.finalize();
+    let out: [u8; 32] = result.try_into().unwrap();
+    out == hashlock
+}
+
+pub fn gen_lock(secret_key: SecretKey) -> HashLock {
+    let mut hasher = Sha3_256::new();
+    hasher.update(secret_key);
     let result = hasher.finalize();
     let out: [u8; 32] = result.try_into().unwrap();
     out
@@ -200,6 +205,20 @@ mod tests {
             .duration_since(SystemTime::UNIX_EPOCH)
             .unwrap();
         let five_seconds_later = now + Duration::new(5, 0);
-        let _transfer_out = contract.fund(sender, receiver, 1, [0; 32], five_seconds_later);
+        let _fund_contract = contract.fund(sender, receiver, 1, [0; 32], five_seconds_later);
+    }
+
+    #[test]
+    fn lock_mechanism() {
+        let key = b"ssssssssssssssssssssssssssssssss";
+        let lock = gen_lock(*key);
+        assert_eq!(
+            [
+                165, 152, 132, 76, 216, 153, 182, 114, 45, 89, 20, 251, 170, 95, 204, 77, 214, 166,
+                43, 58, 171, 243, 206, 181, 109, 46, 63, 177, 197, 13, 234, 154
+            ],
+            lock
+        );
+        assert!(try_lock(*key, lock));
     }
 }
