11a893582d0af469eda4928c8edb3c05af9834f5
Antonio Yang <yanganto@gmail.com>
2022-08-07 19:50:08 +0800

    Add confirm for integration test

--- near/examples/round-trip.rs
+++ near/examples/round-trip.rs
@@ -35,35 +35,80 @@ mod test {
     ) -> Result<Contract> {
         let wasm = std::fs::read("../target/wasm32-unknown-unknown/release/near_atomic_swap.wasm")?;
         let contract = worker.dev_deploy(&wasm).await?;
+
         let res = contract
             .call(&worker, "fund")
             .args_json((sender, receiver, amount, hashlock, timelock))?
             .gas(300_000_000_000_000)
-            .deposit(1001)
+            .deposit(2)
             .transact()
             .await?;
         assert!(res.is_success());
-        println!("{res:?}");
 
-        return Ok(contract);
+        Ok(contract)
+    }
+
+    async fn confirm(
+        worker: &Worker<impl DevNetwork>,
+        contract: Contract,
+        sender: AccountId,
+        receiver: AccountId,
+        amount: Balance,
+        hashlock: [u8; 32],
+        timelock: u64,
+        secret_key: [u8; 32],
+    ) -> Result<Contract> {
+        let res = contract
+            .call(&worker, "confirm")
+            .args_json((sender, receiver, amount, hashlock, timelock, secret_key))?
+            .gas(300_000_000_000_000)
+            .transact()
+            .await?;
+        assert!(res.is_success());
+
+        Ok(contract)
     }
 
     #[test_with::file("target/wasm32-unknown-unknown/release/near_atomic_swap.wasm")]
     #[tokio::test]
     async fn round_trip() -> Result<()> {
         let worker = workspaces::sandbox().await?;
-        let _contract = init_fund(
+        let timelock = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+        let contract = init_fund(
             &worker,
-            "sender".parse().unwrap(),
+            "caller".parse().unwrap(),
             "receiver".parse().unwrap(),
-            1000,
-            [0; 32],
-            SystemTime::now()
-                .duration_since(SystemTime::UNIX_EPOCH)
-                .unwrap()
-                .as_secs(),
+            1,
+            [
+                165, 152, 132, 76, 216, 153, 182, 114, 45, 89, 20, 251, 170, 95, 204, 77, 214, 166,
+                43, 58, 171, 243, 206, 181, 109, 46, 63, 177, 197, 13, 234, 154,
+            ],
+            timelock,
         )
         .await?;
+
+        confirm(
+            &worker,
+            contract,
+            "caller".parse().unwrap(),
+            "receiver".parse().unwrap(),
+            1,
+            [
+                165, 152, 132, 76, 216, 153, 182, 114, 45, 89, 20, 251, 170, 95, 204, 77, 214, 166,
+                43, 58, 171, 243, 206, 181, 109, 46, 63, 177, 197, 13, 234, 154,
+            ],
+            timelock,
+            *b"ssssssssssssssssssssssssssssssss",
+        )
+        .await?;
+
+        let receiver_account: AccountId = "receiver".parse().unwrap();
+        let account_detail = worker.view_account(&receiver_account).await?;
+        assert_eq!(account_detail.balance, 2);
+
         Ok(())
     }
 }
