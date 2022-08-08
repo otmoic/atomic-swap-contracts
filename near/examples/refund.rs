929a3cb88efdb3e16c9be89666042e402277fa8d
Antonio Yang <yanganto@gmail.com>
2022-08-08 14:46:40 +0800

    Add refund test case (#8)

---
+++ near/examples/refund.rs
@@ -0,0 +1,98 @@
+fn main() {}
+
+#[cfg(test)]
+mod test {
+    use anyhow::Result;
+    use std::time::SystemTime;
+    use workspaces::prelude::*;
+    use workspaces::{types::Balance, AccountId, Contract, DevNetwork, Worker};
+
+    async fn init(worker: &Worker<impl DevNetwork>) -> Result<Contract> {
+        let wasm = std::fs::read("../target/wasm32-unknown-unknown/release/near_atomic_swap.wasm")?;
+        let contract = worker.dev_deploy(&wasm).await?;
+        Ok(contract)
+    }
+
+    async fn fund(
+        worker: &Worker<impl DevNetwork>,
+        contract: &Contract,
+        sender: AccountId,
+        receiver: AccountId,
+        amount: Balance,
+        hashlock: [u8; 32],
+        timelock: u64,
+    ) -> Result<()> {
+        let res = contract
+            .call(&worker, "fund")
+            .args_json((sender, receiver, amount, hashlock, timelock))?
+            .gas(300_000_000_000_000)
+            .deposit(2)
+            .transact()
+            .await?;
+        assert!(res.is_success());
+
+        Ok(())
+    }
+
+    async fn refund(
+        worker: &Worker<impl DevNetwork>,
+        contract: &Contract,
+        sender: AccountId,
+        receiver: AccountId,
+        amount: Balance,
+        hashlock: [u8; 32],
+        timelock: u64,
+    ) -> Result<()> {
+        let res = contract
+            .call(&worker, "refund")
+            .args_json((sender, receiver, amount, hashlock, timelock))?
+            .gas(300_000_000_000_000)
+            .transact()
+            .await?;
+        assert!(res.is_success());
+
+        Ok(())
+    }
+
+    #[test_with::file("target/wasm32-unknown-unknown/release/near_atomic_swap.wasm")]
+    #[tokio::test]
+    async fn test_refund() -> Result<()> {
+        let worker = workspaces::sandbox().await?;
+        let timelock = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+
+        let contract = init(&worker).await?;
+
+        fund(
+            &worker,
+            &contract,
+            "caller".parse().unwrap(),
+            "receiver".parse().unwrap(),
+            1,
+            [
+                165, 152, 132, 76, 216, 153, 182, 114, 45, 89, 20, 251, 170, 95, 204, 77, 214, 166,
+                43, 58, 171, 243, 206, 181, 109, 46, 63, 177, 197, 13, 234, 154,
+            ],
+            timelock,
+        )
+        .await?;
+
+        refund(
+            &worker,
+            &contract,
+            "caller".parse().unwrap(),
+            "receiver".parse().unwrap(),
+            1,
+            [
+                165, 152, 132, 76, 216, 153, 182, 114, 45, 89, 20, 251, 170, 95, 204, 77, 214, 166,
+                43, 58, 171, 243, 206, 181, 109, 46, 63, 177, 197, 13, 234, 154,
+            ],
+            timelock,
+        )
+        .await?;
+
+        Ok(())
+    }
+}
