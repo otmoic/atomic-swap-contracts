17ca6fb9e936a89e806c79a9bd0e6c40be4f3ad1
Antonio Yang <yanganto@gmail.com>
2022-08-10 11:26:41 +0800

    Add utils crate

--- near/src/lib.rs
+++ near/src/lib.rs
@@ -5,9 +5,9 @@ use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::{env, log, near_bindgen, require, AccountId, Balance, Promise};
 use sha3::{Digest, Sha3_256};
 
+use utils::{try_lock, HashLock, SecretKey};
+
 pub type TransferId = [u8; 32];
-pub type HashLock = [u8; 32];
-pub type SecretKey = [u8; 32];
 
 /// The constant fee to platform for each transfer
 const FEE: Balance = 1;
@@ -160,22 +160,6 @@ fn keccak256(
     out
 }
 
-fn try_lock(secret_key: SecretKey, hashlock: HashLock) -> bool {
-    let mut hasher = Sha3_256::new();
-    hasher.update(secret_key);
-    let result = hasher.finalize();
-    let out: [u8; 32] = result.try_into().unwrap();
-    out == hashlock
-}
-
-pub fn gen_lock(secret_key: SecretKey) -> HashLock {
-    let mut hasher = Sha3_256::new();
-    hasher.update(secret_key);
-    let result = hasher.finalize();
-    let out: [u8; 32] = result.try_into().unwrap();
-    out
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -206,19 +190,5 @@ mod tests {
         let five_seconds_later = now + Duration::new(5, 0);
         let _fund_contract =
             contract.fund(sender, receiver, 1, [0; 32], five_seconds_later.as_secs());
-    }
-
-    #[test]
-    fn lock_mechanism() {
-        let key = b"ssssssssssssssssssssssssssssssss";
-        let lock = gen_lock(*key);
-        assert_eq!(
-            [
-                165, 152, 132, 76, 216, 153, 182, 114, 45, 89, 20, 251, 170, 95, 204, 77, 214, 166,
-                43, 58, 171, 243, 206, 181, 109, 46, 63, 177, 197, 13, 234, 154
-            ],
-            lock
-        );
-        assert!(try_lock(*key, lock));
     }
 }
--- solana/src/lib.rs
+++ solana/src/lib.rs
@@ -1,12 +1,20 @@
+use std::collections::HashMap;
+
 use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::{
     account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey,
 };
-use std::collections::HashMap;
+
+use utils::{HashLock, SecretKey};
 
 pub type TransferId = [u8; 32];
-pub type HashLock = [u8; 32];
-pub type SecretKey = [u8; 32];
+
+#[derive(BorshDeserialize, BorshSerialize)]
+pub enum Methods {
+    Fund(Pubkey, Pubkey, u64, HashLock, u64),
+    Confirm(Pubkey, Pubkey, u64, HashLock, u64, SecretKey),
+    Refund(Pubkey, Pubkey, u64, HashLock, u64),
+}
 
 #[derive(BorshDeserialize, BorshSerialize, PartialEq, Debug)]
 pub enum TransferStatus {
@@ -25,8 +33,15 @@ entrypoint!(atomic_swap);
 pub fn atomic_swap(
     _program_id: &Pubkey,
     _accounts: &[AccountInfo],
-    _instruction_data: &[u8],
+    instruction_data: &[u8],
 ) -> ProgramResult {
-    msg!("Atomic swap entrypoint");
+    if let Ok(method) = Methods::try_from_slice(instruction_data) {
+        match method {
+            _ => (),
+        }
+    } else {
+        msg!("Unsupport method");
+    }
+
     Ok(())
 }
