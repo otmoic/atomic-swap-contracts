a0d0313b9df2c2ee0daf5eea02fc0734ec9fad11
Antonio Yang <yanganto@gmail.com>
2022-08-15 14:24:58 +0800

    Add sanity tests

--- solana/src/lib.rs
+++ solana/src/lib.rs
@@ -1,3 +1,4 @@
+#![feature(derive_default_enum)]
 use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
@@ -26,22 +27,23 @@ pub enum Method {
     Refund(Pubkey, Pubkey, u64, HashLock, u64),
 }
 
-#[derive(BorshDeserialize, BorshSerialize, PartialEq, Debug)]
+#[derive(BorshDeserialize, BorshSerialize, PartialEq, Debug, Default)]
 pub enum TransferStatus {
+    #[default]
     Initializd,
     Pending,
     Confirmed,
     Refunded,
 }
 
-#[derive(BorshSerialize, BorshDeserialize, Debug)]
+#[derive(BorshSerialize, BorshDeserialize, Debug, Default)]
 pub struct Storage {
     pub sender: Pubkey,
     pub receiver: Pubkey,
     pub amount: u64,
     pub hashlock: HashLock,
     pub timelock: u64,
-    pub secret_key: Option<SecretKey>,
+    pub secret_key: SecretKey,
     pub status: TransferStatus,
 }
 
@@ -72,6 +74,7 @@ fn fund(
     if account.owner == program_id {
         invoke(&transfer(sender, &PLATFORM, FEE), &[account.clone()])?;
         invoke(&transfer(sender, program_id, amount), &[account.clone()])?;
+        msg!("transfer invoke called");
         let mut storage = Storage::try_from_slice(&account.data.borrow())?;
         if storage.status == TransferStatus::Initializd {
             storage.sender = *sender;
@@ -79,6 +82,7 @@ fn fund(
             storage.amount = amount;
             storage.hashlock = hashlock;
             storage.timelock = timelock;
+            storage.status = TransferStatus::Pending;
             storage.serialize(&mut &mut account.data.borrow_mut()[..])?;
             Ok(())
         } else {
@@ -113,7 +117,7 @@ fn confirm(
                 && storage.timelock == timelock
             {
                 invoke(&transfer(program_id, receiver, amount), &[account.clone()])?;
-                storage.secret_key = Some(secret_key);
+                storage.secret_key = secret_key;
                 storage.status = TransferStatus::Confirmed;
                 storage.serialize(&mut &mut account.data.borrow_mut()[..])?;
                 Ok(())
@@ -186,4 +190,139 @@ pub fn atomic_swap(
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use solana_program::clock::Epoch;
+    use std::time::{Duration, SystemTime};
+
+    #[test]
+    fn sanity_round_trip() {
+        let program_id = Pubkey::default();
+        let sender_key = Pubkey::default();
+        let mut lamports = 0;
+        let mut data = Storage::default().try_to_vec().unwrap();
+        let owner = Pubkey::default();
+        let account = AccountInfo::new(
+            &sender_key,
+            false,
+            true,
+            &mut lamports,
+            &mut data,
+            &owner,
+            false,
+            Epoch::default(),
+        );
+        let receiver_key = Pubkey::new_from_array([1; 32]);
+        let now = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap();
+        let five_seconds_later = now + Duration::new(5, 0);
+
+        let method = Method::Fund(
+            sender_key,
+            receiver_key,
+            100,
+            [
+                165, 152, 132, 76, 216, 153, 182, 114, 45, 89, 20, 251, 170, 95, 204, 77, 214, 166,
+                43, 58, 171, 243, 206, 181, 109, 46, 63, 177, 197, 13, 234, 154,
+            ],
+            five_seconds_later.as_secs(),
+        );
+        let instruction_data: Vec<u8> = method.try_to_vec().unwrap();
+
+        let accounts = vec![account];
+
+        let mut storage = Storage::try_from_slice(&accounts[0].data.borrow()).unwrap();
+        assert_eq!(storage.status, TransferStatus::Initializd);
+
+        atomic_swap(&program_id, &accounts, &instruction_data).unwrap();
+
+        storage = Storage::try_from_slice(&accounts[0].data.borrow()).unwrap();
+        assert_eq!(storage.status, TransferStatus::Pending);
+
+        let method = Method::Confirm(
+            sender_key,
+            receiver_key,
+            100,
+            [
+                165, 152, 132, 76, 216, 153, 182, 114, 45, 89, 20, 251, 170, 95, 204, 77, 214, 166,
+                43, 58, 171, 243, 206, 181, 109, 46, 63, 177, 197, 13, 234, 154,
+            ],
+            five_seconds_later.as_secs(),
+            *b"ssssssssssssssssssssssssssssssss",
+        );
+        let instruction_data: Vec<u8> = method.try_to_vec().unwrap();
+
+        atomic_swap(&program_id, &accounts, &instruction_data).unwrap();
+
+        storage = Storage::try_from_slice(&accounts[0].data.borrow()).unwrap();
+        assert_eq!(storage.status, TransferStatus::Confirmed);
+    }
+
+    #[test]
+    fn incorrect_secret() {
+        let program_id = Pubkey::default();
+        let sender_key = Pubkey::default();
+        let mut lamports = 0;
+        let mut data = Storage::default().try_to_vec().unwrap();
+        let owner = Pubkey::default();
+        let account = AccountInfo::new(
+            &sender_key,
+            false,
+            true,
+            &mut lamports,
+            &mut data,
+            &owner,
+            false,
+            Epoch::default(),
+        );
+        let receiver_key = Pubkey::new_from_array([1; 32]);
+        let now = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap();
+        let five_seconds_later = now + Duration::new(5, 0);
+
+        let method = Method::Fund(
+            sender_key,
+            receiver_key,
+            100,
+            [
+                165, 152, 132, 76, 216, 153, 182, 114, 45, 89, 20, 251, 170, 95, 204, 77, 214, 166,
+                43, 58, 171, 243, 206, 181, 109, 46, 63, 177, 197, 13, 234, 154,
+            ],
+            five_seconds_later.as_secs(),
+        );
+        let instruction_data: Vec<u8> = method.try_to_vec().unwrap();
+
+        let accounts = vec![account];
+
+        let mut storage = Storage::try_from_slice(&accounts[0].data.borrow()).unwrap();
+        assert_eq!(storage.status, TransferStatus::Initializd);
+
+        atomic_swap(&program_id, &accounts, &instruction_data).unwrap();
+
+        storage = Storage::try_from_slice(&accounts[0].data.borrow()).unwrap();
+        assert_eq!(storage.status, TransferStatus::Pending);
+
+        let method = Method::Confirm(
+            sender_key,
+            receiver_key,
+            100,
+            [
+                165, 152, 132, 76, 216, 153, 182, 114, 45, 89, 20, 251, 170, 95, 204, 77, 214, 166,
+                43, 58, 171, 243, 206, 181, 109, 46, 63, 177, 197, 13, 234, 154,
+            ],
+            five_seconds_later.as_secs(),
+            *b"nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn",
+        );
+        let instruction_data: Vec<u8> = method.try_to_vec().unwrap();
+
+        assert_eq!(
+            atomic_swap(&program_id, &accounts, &instruction_data),
+            Err(ProgramError::Custom(Error::SecretNotMatch as u32))
+        );
+    }
 }
